02a60a8893fa65cadb6054392541f56841176c80
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-08-18 19:47:43 +0530

    cancelable debounce & throttle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,48 @@
-export function debounce<F extends (...args: any[]) => any>(func: F, wait: number): (...args: Parameters<F>) => void {
-  let timeout: ReturnType<typeof setTimeout> | null = null;
-  return (...args: Parameters<F>) => {
-    if (timeout !== null) {
-      clearTimeout(timeout);
-    }
-    timeout = setTimeout(() => func(...args), wait);
+type CancellableFunction<T extends (...args: any[]) => any> = {
+  (...args: Parameters<T>): ReturnType<T>;
+  cancel: () => void;
+};
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): CancellableFunction<T> {
+  let timeout: NodeJS.Timeout | null = null;
+
+  const debouncedFunc = function (this: any, ...args: Parameters<T>) {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  } as CancellableFunction<T>;
+
+  debouncedFunc.cancel = () => {
+    if (timeout) clearTimeout(timeout);
   };
+
+  return debouncedFunc;
 }
 
-export function throttle<F extends (...args: any[]) => any>(func: F, limit: number): (...args: Parameters<F>) => void {
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): CancellableFunction<T> {
   let inThrottle = false;
-  return (...args: Parameters<F>) => {
+  let lastFunc: NodeJS.Timeout | null = null;
+
+  const throttledFunc = function (this: any, ...args: Parameters<T>) {
     if (!inThrottle) {
-      func(...args);
+      func.apply(this, args);
       inThrottle = true;
       setTimeout(() => (inThrottle = false), limit);
+    } else {
+      if (lastFunc) clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => func.apply(this, args), limit);
     }
+  } as CancellableFunction<T>;
+
+  throttledFunc.cancel = () => {
+    if (lastFunc) clearTimeout(lastFunc);
+    inThrottle = false;
   };
+
+  return throttledFunc;
 }
